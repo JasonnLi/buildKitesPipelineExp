b7387ee08fd5efb0a327e56574107a766a9e4ca4
JasonnLi <jasonlee5233@gmail.com>
2021-07-04 20:13:25 +1000

    init commit

---
+++ client/src/components/App.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+
+const App = () => {
+
+return (
+    <div className="wrapper">
+        <h1>React Frontend for Devops Testing!🚀</h1>
+    </div>
+    );
+};
+
+export default App;
